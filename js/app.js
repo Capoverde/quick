20bd4af7bd8db4cb52a81893d54c12f7ac613f14
Piotr <piotr.slobinski@gmail.com>
2020-01-15 21:42:51 +0100

    quick v.0.0.1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(function () {
+  console.log('#########################################')
+  console.log('######## jQuery 3.4.1.slim.min.js #######')
+  console.log('#########################################')
+
+  /*  ------ aos object --------
+   link to documentation: https://github.com/michalsnik/aos
+  */
+  AOS.init()
+})
