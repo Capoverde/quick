271811cc264247efbad92c91a51166be902d6b4c
Piotr <piotr.slobinski@gmail.com>
2020-01-16 02:08:44 +0100

    version 0.0.1

--- js/app.js
+++ js/app.js
@@ -7,4 +7,35 @@ $(function () {
    link to documentation: https://github.com/michalsnik/aos
   */
   AOS.init()
+
+  // ----- totop ------ //
+  if ($('#totop').length) {
+    var scrollTrigger = 300; // px
+      var backToTop = function () {
+        var scrollTop = $(window).scrollTop()
+        if (scrollTop > scrollTrigger) {
+          $('#totop').addClass('showTop')
+        } else {
+          $('#totop').removeClass('showTop')
+        }
+      }
+    backToTop()
+    $(window).on('scroll', function () {
+      backToTop()
+    })
+    $('#totop').on('click', function (e) {
+      e.preventDefault()
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700)
+    })
+  }
+
+  // ------ smooth scroll to secion
+  $(document).on('click', 'a', function (event) {
+    event.preventDefault()
+    $('html,body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800)
+  })
 })
